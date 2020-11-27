92b10776bb73a3bb18dbbf191490ef40791aa1b6
Scott Davies <scottdavies1983@gmail.com>
2020-11-27 19:00:21 +0000

    sarcastic function set

--- exercises/56 - Sarcastic Text/text.js
+++ exercises/56 - Sarcastic Text/text.js
@@ -3,15 +3,23 @@ const result = document.querySelector(".result");
 // eslint-disable-next-line quotes
 const filterInputs = document.querySelectorAll('[name="filter"]');
 
-function transformText(text) {
-  // take the text and loop over each letter
-  result.textContent = text;
-}
-
 const filters = {
-  sarcastic() {},
+  sarcastic(letter, index) {
+    if (index % 2) {
+      return letter.toUpperCase();
+    }
+    // so if it is even it will return lowercase
+    return letter.toLowerCase();
+  },
   funky() {},
   unable() {},
 };
+
+function transformText(text) {
+  // take the text and loop over each letter
+  const mod = Array.from(text).map(filters.sarcastic);
+  console.log(mod);
+  result.textContent = mod.join();
+}
 
 textarea.addEventListener("input", (e) => transformText(e.target.value));
